773d3165ea8d2ef6a5c4ed9dbf53d5365bb9e511
Bryan Harter <harter@lasp.colorado.edu>
2016-04-15 16:19:19 -0600

    Still working on unit tests

--- tests/test_python_divide.py
+++ tests/test_python_divide.py
@@ -24,16 +24,21 @@ class TestPythonDivide(unittest.TestCase):
         self.x = 5
 
     def tearDown(self):
-        self.x = 2
+        for f in os.listdir(os.path.join(get_root_data_dir(),'maven','data','sci','kp','insitu','2015','01')):
+            os.remove(f)
+        for f in os.listdir(os.path.join(get_root_data_dir(),'maven','data','sci','euv','l3','2015','01')):
+            os.remove(f)
 
     def test_download_kp_files(self):
         mvn_kp_download_files(start_date='2015-01-05', end_date='2015-01-10', unittest=True)
-        x = os.listdir(get_root_data_dir())
+        x = os.listdir(os.path.join(get_root_data_dir(),'maven','data','sci','kp','insitu','2015','01'))
         self.assertEqual(len(x),5)
         
     def test_download_sci_files(self):
         mvn_kp_download_sci_files(start_date='2015-01-05', end_date='2015-01-10', instrument='euv', level='l3', unittest=True)
-    
+        x = os.listdir(os.path.join(get_root_data_dir(),'maven','data','sci','euv','l3','2015','01'))
+        self.assertEqual(len(x),5)
+        
     def test_read_in_kp_files(self):
         insitu = mvn_kp_read(['2015-01-05', '2015-01-08T05:15:00'], instruments=['SWEA','NGIMS','MAG'])
 
