b79e3ef5f3a67411d689c6e9029c1c4e85c8d35d
Bryan Harter <harter@lasp.colorado.edu>
2016-04-28 16:30:27 -0600

    mvn_kp_bin is complete

--- mvn_kp_bin.py
+++ mvn_kp_bin.py
@@ -29,6 +29,19 @@ def mvn_kp_bin(kp,
     if avg==False and std==False and median==False and density==False:
         print "Must select array(s) to return (avg, std, median, density)."
         return
+    
+    if not hasattr(binsizes, "__len__"):
+        temp = []
+        temp.append(binsizes)
+        binsizes = temp
+    if mins != None and not hasattr(mins, "__len__"):
+        temp = []
+        temp.append(mins)
+        mins = temp
+    if maxs != None and not hasattr(maxs, "__len__"):
+        temp = []
+        temp.append(maxs)
+        maxs = temp
     #
     # Store instrument and observation of parameter in lists
     #
@@ -72,13 +85,21 @@ def mvn_kp_bin(kp,
     if mins == None:
         mins = []
         for inst, obs in bin_by_inst_obs:
-            mins.append(kp[inst][obs].min(skipna=True))
+            min_temp = kp[inst][obs].min(skipna=True)
+            if math.isnan(min_temp):
+                print "All " + obs + " data is NaN.  Cannot bin by this parameter."
+                return 
+            mins.append(min_temp)
     if maxs == None:
         maxs = []
         for inst, obs in bin_by_inst_obs:
             maxs.append(kp[inst][obs].max(skipna=True))
         
-    for i in range(total_fields):        
+    for i in range(total_fields):
+        if maxs[i]-mins[i] < 0:
+            print "ERROR: Minimum value of " + str(mins[i]) + " is greater than the maximum value of " + str(maxs[i])       
+            print "for bin-by parameter " + bin_by_inst_obs[i][1] + ".  Returning..."
+            return
         ranges.append(maxs[i]-mins[i])
         total_bins.append(int(math.ceil(ranges[i]/binsizes[i])))
     
@@ -87,7 +108,7 @@ def mvn_kp_bin(kp,
     #Initialize the density array (the number of values binned into a bin)
     #
     binned_array = numpy.zeros(total_bins)
-    density = numpy.zeros(total_bins)
+    density_array = numpy.zeros(total_bins)
     binned_list = binned_array.tolist()
     binned_list  = initialize_list(binned_list)
     
@@ -96,6 +117,7 @@ def mvn_kp_bin(kp,
     #Loop through the KP to place the data into the correct bin
     #
     for i in range(len(kp[parameter_inst_obs[0][0]][parameter_inst_obs[0][1]])):
+        bad_val=False
         #
         #Cannot do anything with NaNs.  Ignore them and continue.  
         #
@@ -108,16 +130,22 @@ def mvn_kp_bin(kp,
         data_value_indexes = []
         for bin_by_inst, bin_by_obs in bin_by_inst_obs:
             data_value = kp[bin_by_inst][bin_by_obs][i]
+            #Ignore if NaN or out of range
+            if math.isnan(data_value) or data_value < mins[j] or data_value > maxs[j]:
+                bad_val=True
+                continue
             dv = math.floor((data_value-mins[j])/binsizes[j])
             data_value_indexes.append(int(dv))
             j = j + 1
-        
+            
+        if bad_val:
+            continue
         #
         #Populate binned_list in the proper spot, and add one to the density at that spot
         #
         data_value_indexes = tuple(data_value_indexes)
         place_values_in_list(binned_list, data_value_indexes, kp[parameter_inst_obs[0][0]][parameter_inst_obs[0][1]][i])
-        density[data_value_indexes] = density[data_value_indexes] + 1
+        density_array[data_value_indexes] = density_array[data_value_indexes] + 1
     
     #
     #Create arrays based on keywords
@@ -138,6 +166,7 @@ def mvn_kp_bin(kp,
     #in each bin first.
     #
     for i in range(len(kp[parameter_inst_obs[0][0]][parameter_inst_obs[0][1]])):
+        bad_val=False
         #
         #Cannot do anything with NaNs.  Ignore them and continue.  
         #
@@ -151,10 +180,16 @@ def mvn_kp_bin(kp,
         
         for bin_by_inst, bin_by_obs in bin_by_inst_obs:
             data_value = kp[bin_by_inst][bin_by_obs][i]
+            #Ignore if NaN or out of range
+            if math.isnan(data_value) or data_value < mins[j] or data_value > maxs[j]:
+                bad_val=True
+                continue
             dv = math.floor((data_value-mins[j])/binsizes[j])
             data_value_indexes.append(int(dv))
             j = j + 1
-        
+            
+        if bad_val:
+            continue
         #
         #Calculate the mean/median/mode from the values in "output_list"
         #
@@ -162,21 +197,27 @@ def mvn_kp_bin(kp,
         if median:
             median_array[data_value_indexes] = numpy.nanmedian(get_values_from_list(binned_list, data_value_indexes))
         if avg or std:
-            average_array[data_value_indexes] = numpy.nansum(get_values_from_list(binned_list, data_value_indexes))/density[data_value_indexes]
+            average_array[data_value_indexes] = numpy.nansum(get_values_from_list(binned_list, data_value_indexes))/density_array[data_value_indexes]
         if std:
             squared_total = []
             for x in get_values_from_list(binned_list, data_value_indexes):
                 squared_total.append((x-average_array[data_value_indexes])*(x-average_array[data_value_indexes]))
-            std_array[data_value_indexes] = numpy.sqrt((numpy.sum(squared_total)/density[data_value_indexes]))
+            std_array[data_value_indexes] = numpy.sqrt((numpy.sum(squared_total)/density_array[data_value_indexes]))
             
     #RETURN MEDIAN/AVERAGE/STANDARD DEVIATION
     return_list = []
     if median:
         return_list.append(median_array)
+        print 'Returning binned Medians'
     if avg:
         return_list.append(average_array)
+        print 'Returning binned Averages'
     if std:
         return_list.append(std_array)
+        print 'Returning binned standard deviations'
+    if density:
+        return_list.append(density_array)
+        print 'Returning binned densities'
     #
     #Print out a little cheat sheet so people know what is in the array they're getting
     #
--- tests/test_python_divide.py
+++ tests/test_python_divide.py
@@ -11,7 +11,9 @@ from mvn_kp_download_files import mvn_kp_download_files
 from mvn_kp_download_files_utilities import get_root_data_dir
 from mvn_kp_download_sci_files import mvn_kp_download_sci_files
 from mvn_kp_read import mvn_kp_read
-
+from mvn_kp_bin import mvn_kp_bin
+import math
+import numpy
 
 class TestPythonDivide(unittest.TestCase):
     '''
@@ -43,4 +45,11 @@ class TestPythonDivide(unittest.TestCase):
         mvn_kp_download_files(start_date='2015-01-05', end_date='2015-01-10', unittest=True)
         insitu = mvn_kp_read(['2015-01-05', '2015-01-08T05:15:00'], instruments=['SWEA','NGIMS','MAG'])
         self.assertEqual(len(insitu['MAG']['Magnetic Field MSO X']),37715)
+        
+    def test_bin_kp_data(self):
+        #Basically just here to make sure bin is runing properly, really hard to actually check answers
+        mvn_kp_download_files(start_date='2015-01-15', end_date='2015-01-15', unittest=True)   
+        insitu = mvn_kp_read('2015-01-15')
+        x,y,z,q = mvn_kp_bin(insitu, parameter='SEP.Ion Flux FOV 1 F', bin_by=['SWEA.Solar Wind Electron Density', 'SWEA.Solar Wind Electron Temperature', 'SPACECRAFT.Altitude Aeroid'], binsizes=[1,1,1000], median=True, avg=True, std=True, density=True)
+        self.assertEqual(int(math.floor(numpy.nansum(z))), 108921)
 
