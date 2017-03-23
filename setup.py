585bca0f8bf2aa70cd04193ff42d6429cb194cc8
Bryan Harter <harter@lasp.colorado.edu>
2017-03-23 17:18:28 -0600

    New Version of PYDIVIDE

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
         return f.read()
 
 setup(name='pydivide',
-      version='0.0.3',
+      version='0.0.4',
       description='A tool to plot MAVEN Key Parameter data',
       url='http://github.com/MAVENSDC/pydivide',
       author='MAVEN SDC',
